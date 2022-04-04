f7b91af733bce1e4ce0f0656e84166b4db1147d1
Mario Rovito <alessandro.r14@gmail.com>
2022-04-04 18:53:09 +0200

    add function for add email in html

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 const app = new Vue ({
 
+    el: '#app',
     data :
     {
         email : []
     },
-
+    //utilizzo la funzione mounted, ciclo tramite axios l'api fin quando non arrivo a 10. una volta ciclato pusherò le mail generate nell'array
+    mounted(){
+        for (let i = 0; i < 10; i++){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(element =>{
+                   // console.log(element.data.response);
+                    this.email.push(element.data.response);
+                    //console.log(this.email);
+                })
+        }
+    }
 
 })
