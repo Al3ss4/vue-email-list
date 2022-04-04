18ab142dbf680a0f64746e8a0820827a4d203e77
Mario Rovito <alessandro.r14@gmail.com>
2022-04-04 17:07:35 +0200

    add array email and id app in html

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('JS OK!');
+
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+
+const app = new Vue ({
+
+    data :
+    {
+        email : []
+    },
+
+
+})
